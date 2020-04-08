aa8c6c33425bb9728cb9574e184d9217c88f039e
TheBlocks-CN <KujighBlockYYX@gmail.com>
2020-04-08 17:55:41 +0800

    迫害StarLight繁星工坊服务器的腐竹:贺兰星辰。 这个服务器的MCBBS宣传贴:https://www.mcbbs.net/forum.php?mod=viewthread&tid=915693 这个服务器的MCMOD宣传贴:https://play.mcmod.cn/sv20183254.html 服务器官网:www.minecraft.kim

--- HLXC PH v1.1.py
+++ HLXC PH v1.1.py
@@ -2,29 +2,29 @@ Language = input("What language r u use?zh-CN(zh-SG) , zh-TW(zh-HK,zh-MO) or en-
 Language = int(Language)
 
 if Language == 1:
-    password = input("Please type the Password!")
-    password = int(password)
-    if password == 459279:
+    digital = input("Please type the digital!")
+    digital = int(digital)
+    if digital == 459279:
         print("恭喜你，成功输入了密码!")
         print("这串数字的意思是:贺兰是屑!")
 
     else:
-        print('The password you entered is incorrect! Please enter again!')
+        print('The digital you entered is incorrect! Please enter again!')
 
 if Language == 2:
-    password = input("Please type the Password!")
-    password = int(password)
-    if password == 459279:
+    digital = input("Please type the digital!")
+    digital = int(digital)
+    if digital == 459279:
         print("恭喜你，成功輸入了密碼！")
         print("這串數字的意思是:賀蘭是屑!")
     else:
-        print('The password you entered is incorrect! Please enter again!')
+        print('The digital you entered is incorrect! Please enter again!')
 
 if Language == 3:
-    password = input("Please type the Password!")
-    password = int(password)
-    if password == 459279:
-        print("Congratulations,ur type's password is right!")
+    digital = input("Please type the digital!")
+    digital = int(digital)
+    if digital == 459279:
+        print("Congratulations,ur type's digital is right!")
         print("This int's mean : shaokeyibb is xie!")
     else:
-        print('The password you entered is incorrect! Please enter again!')
+        print('The digital you entered is incorrect! Please enter again!')
