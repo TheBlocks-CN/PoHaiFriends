e22cff36393b6a52c565f75768e543a0d12fe77c
TheBlocks-CN <KujighBlockYYX@gmail.com>
2020-04-08 17:50:22 +0800

    迫害StarLight繁星工坊服务器的腐竹:贺兰星辰。 这个服务器的MCBBS宣传贴:https://www.mcbbs.net/forum.php?mod=viewthread&tid=915693 这个服务器的MCMOD宣传贴:https://play.mcmod.cn/sv20183254.html 服务器官网:www.minecraft.kim

---
+++ HLXC PH v1.1.py
@@ -0,0 +1,30 @@
+Language = input("What language r u use?zh-CN(zh-SG) , zh-TW(zh-HK,zh-MO) or en-UK (en-US)?If u use Chinese(Simplified) type 1,use Chinese(Tranditional) type 2, use English type 3.")
+Language = int(Language)
+
+if Language == 1:
+    password = input("Please type the Password!")
+    password = int(password)
+    if password == 459279:
+        print("恭喜你，成功输入了密码!")
+        print("这串数字的意思是:贺兰是屑!")
+
+    else:
+        print('The password you entered is incorrect! Please enter again!')
+
+if Language == 2:
+    password = input("Please type the Password!")
+    password = int(password)
+    if password == 459279:
+        print("恭喜你，成功輸入了密碼！")
+        print("這串數字的意思是:賀蘭是屑!")
+    else:
+        print('The password you entered is incorrect! Please enter again!')
+
+if Language == 3:
+    password = input("Please type the Password!")
+    password = int(password)
+    if password == 459279:
+        print("Congratulations,ur type's password is right!")
+        print("This int's mean : shaokeyibb is xie!")
+    else:
+        print('The password you entered is incorrect! Please enter again!')
