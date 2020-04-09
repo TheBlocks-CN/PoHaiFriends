356be26f2bdb7a9d5d59aa76bec1b29f472fb22d
TheBlocks-CN <KujighBlockYYX@gmail.com>
2020-04-09 14:58:45 +0800

    Mical爬去写作业!!!

---
+++ Mical go to do homework!.py
@@ -0,0 +1,14 @@
+Language = input("What language r u use?zh-CN(zh-SG) , zh-TW(zh-HK,zh-MO) or en-UK (en-US)?If u use Chinese(Simplified) type zh-CN,use Chinese(Tranditional) type zh-TW, use English type en-UK.")
+
+if Language == 'zh-CN':
+    print("Mical爬去写作业!")
+
+if Language == 'zh-TW':
+    print("Mical爬去寫作業!")
+
+if Language == 'en-UK':
+    print("Mical go to do homework!")
+
+input()
+while True:
+    print("Mical爬去写作业!!!")
