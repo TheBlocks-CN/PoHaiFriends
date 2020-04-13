fb3cf74661eb2401faaeb233f44510b018644d21
TheBlocks-CN <KujighBlockYYX@gmail.com>
2020-04-13 11:08:15 +0800

    该版本将语言代码改成了"zh-cmn-Hans"等识别方式。

---
+++ LCL PH v1.4.py
@@ -0,0 +1,33 @@
+print("If you type error,Please Run this py again.")
+print("该脚本严格区分大小写，请正确输入'zh-cmn-Hans'等语言代码")
+Language = input("What language r u use?zh-cmn-Hans(zh-SG) , zh-cmn-Hant(zh-HK,zh-MO) or English?If u use Chinese(Simplified) type zh-cmn-Hans,use Chinese(Tranditional) type zh-cmn-Hant, use English type en.")
+
+if Language == 'zh-cmn-Hans':
+    letter = input("Please input the letter")
+    if letter == 'lclsx':
+        print("恭喜你!你正确输入了字母!")
+        print("'lclsx'的意思是:黎程朗是屑!")
+    else:
+        print("你输入的字母呢错误!请再次输入!")
+
+
+if Language == 'zh-cmn-Hant':
+    letter = input("Please input the letter")
+    if letter == 'lclsx':
+        print("恭喜你!你正確輸入了字母！")
+        print("'lclsx'的意思是:黎程朗是屑！")
+    else:
+        print("你輸入的字母錯誤!請重新輸入!")
+
+if Language == 'en':
+    letter = input("Please type the letter!")
+    if letter == 'lclsx':
+        print("Congratulations!You type letter is right!")
+        print("This letter mean Li Chenglang is xie!")
+    else:
+        print('The letter you entered is incorrect! Please enter again!')
+
+
+#此版本将变量"Language"的识别方式从"1,2,3"改成ISO 639中的"
+
+
